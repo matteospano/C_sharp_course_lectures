31a5e2799459e9a6c866d4bd8b96dd771e3501f0
Matteo Spano <matteospano15@gmail.com>
2024-05-16 22:02:41 +0200

    fine sezione c#

--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e505cd0fc88b4df09b500d15e1320e7cdc317ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30a8eba8c903461bf2534394d5384c5c35b856c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
